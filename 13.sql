dca7c6eee123c70fb728bfcf1c5c544669c62aa0
SScintillate <sahilsandasani@gmail.com>
2022-02-02 23:35:37 -0800

    Add files via upload

---
+++ 13.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT name FROM stars
+JOIN people ON stars.person_id = people.id
+WHERE movie_id IN
+(SELECT movie_id FROM people
+JOIN stars On stars.person_id = people.id
+WHERE birth = 1958 and name = "Kevin Bacon") AND name != "Kevin Bacon";
